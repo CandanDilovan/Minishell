5cc7a6a1b3f1e37cd6b71ffd9a35d295e893c325
Dilovan Candan <dilovancandan@MacBook-Pro-de-Dilovan.local>
2023-07-26 23:47:28 +0200

    ca crash plu

--- src/minishell.c
+++ src/minishell.c
@@ -6,26 +6,38 @@
 /*   By: dilovancandan <dilovancandan@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/25 18:32:05 by dilovancand       #+#    #+#             */
-/*   Updated: 2023/07/26 20:51:04 by dilovancand      ###   ########.fr       */
+/*   Updated: 2023/07/26 23:46:19 by dilovancand      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+char	*test_prompt(char *str)
+{
+	char	*prompt;
+
+	prompt = malloc(sizeof(char) * (ft_strlen(str) + 1));
+	strcpy(prompt, str);
+	return (prompt);
+}
+
 int	main(void)
 {
-	char	*promptt;
+	char	*test;
+	char	*test2;
 
-	promptt = NULL;
-	//rl_bind_key('\t', rl_complete);
+	test = test_prompt("minishell $ ");
+	rl_bind_key('\t', rl_complete);
 	while (1)
 	{
-		promptt = readline("minishell $ ");
-		if (!promptt)
+		test2 = readline(NULL);
+		printf("%s", test);
+		if (test2 == NULL)
 			break ;
-		ft_printf("prout");
-		//add_history(promptt);
-		//free(prompt);
+		else
+		{
+			add_history(test2);
+		}
 	}
 	return (0);
 }
